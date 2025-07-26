f53f6fa221b99743ea14d46185bf0a7438099014
Taus-SW <119143231+Taus-SW@users.noreply.github.com>
2025-07-26 19:06:15 +0100

    feature/monitor-app
    - monitor app
    - updated camera controller
    - message and parameters proto
    - added get and set functions in QHYCameraController

--- src/QHYCamera2ecal.py
+++ src/QHYCamera2ecal.py
@@ -6,26 +6,37 @@
 
 import time
 import multiprocessing as mp
+import cv2
 import argparse
 
+from ecal_lib.ecal_util.proto_receiver import ProtoReceiver
 from ecal_lib.ecal_util.proto_sender import ProtoSender
 from ecal_lib.ecal_util.set_process_name import *
 from camera_controler.QHYCameraController import QHYCameraController
+
+QHY_CHANNEL_NAME = "QHYCamera"
+MESSAGE_NAME = "qhy_camera_image"
+PROTO_FILE = "ecal_lib.proto_files.qhy_camera_image_pb2"
+
+STATUS_MESSAGE_NAME = "qhy_camera_status"
+STATUS_PROTO_FILE = "ecal_lib.proto_files.qhy_camera_status_pb2"
+QHY_STATUS_CHANNEL = "QHYCamera_status"
+
+PARAMS_MESSAGE_NAME = "qhy_camera_parameters"
+PARAMS_PROTO_FILE = "ecal_lib.proto_files.qhy_camera_parameters_pb2"
+QHY_PARAMS_CHANNEL = "QHYCamera_parameters"
 
 MAX_RESTART_ATTEMPTS = 3
 PULSE_TIMEOUT_SEC = 5
 INIT_PULSE_TIMEOUT_SEC = 30
 
-MESSAGE_NAME = "qhy_camera_image"
-PROTO_FILE = "ecal_lib.proto_files.qhy_camera_image_pb2"
-
-def QHYCamera2ecal(channel_name, param_queue, status_queue):
+def QHYCamera2ecal(param_queue, status_queue):
 
     # Set process name
-    set_process_name(f"{channel_name} Broadcast")
+    set_process_name(f"{QHY_CHANNEL_NAME} Broadcast")
 
     # Proto sender
-    proto_snd = ProtoSender(channel_name, MESSAGE_NAME, PROTO_FILE)
+    proto_snd = ProtoSender(QHY_CHANNEL_NAME, MESSAGE_NAME, PROTO_FILE)
 
     # QHY Camera Controller
     qhy_camera = QHYCameraController()
@@ -33,78 +44,75 @@ def QHYCamera2ecal(channel_name, param_queue, status_queue):
     # Infinite loop - main_controller kills the process if it has to terminate
     while True:
         # Capture frame-by-frame
-        img = qhy_camera.get_live_frame()
+        frame = qhy_camera.get_live_frame()
 
         # Check if frame is not empty
-        if img is None:
+        if frame is None:
             print(f"Camera not available")
             break
-        else:
-            protobuf_message = proto_snd.message
-            protobuf_message.width = qhy_camera.get_shape()[1]
-            protobuf_message.height = qhy_camera.get_shape()[0]
-            protobuf_message.bit_per_pixel = 16
-            protobuf_message.raw_image = img.tobytes()
-            protobuf_message.time_stamp = int(time.time() * 1.0e6)
 
-            # Send the message to the topic this publisher was created for
-            proto_snd.send(protobuf_message)
+        # Access the protobuf type definition
+        protobuf_message = proto_snd.message
+        protobuf_message.width = qhy_camera.get_shape()[1]
+        protobuf_message.height = qhy_camera.get_shape()[0]
+        protobuf_message.bit_per_pixel = 16
+        protobuf_message.raw_image = frame.tobytes()
+        protobuf_message.time_stamp = int(time.time() * 1.0e6)
 
-            # send status message
-            status_message = {
-                'pulse_time': time.time(),
-                # TODO: add these get functions in QHYCameraController
-                # 'timestamp': qhy_camera.get_timestamp()
-                # 'exposure': qhy_camera.exposure()
-                # 'gain': qhy_camera.gain()
-                # 'temperature': qhy_camera.temperature()
-            }
-            status_queue.put(status_message)
+        # Send the message to the topic this publisher was created for
+        proto_snd.send(protobuf_message)
 
-            # TODO: add these set functions in QHYCameraController
-            # Maybe pass the not None check in the set function
-            # while not param_queue.empty():
-            #     config_message = param_queue.get()
-            #     if (target_brightness := config_message['target_brightness']) is not None:
-            #         qhy_camera.set_target_brightness(target_brightness)
-            #     if (target_gain := config_message['target_gain']) is not None:
-            #         qhy_camera.set_target_gain(target_gain)
-            #     if (exposure_min := config_message['exposure_min']) is not None:
-            #         qhy_camera.set_exposure_min(exposure_min)
-            #     if (exposure_max := config_message['exposure_max']) is not None:
-            #         qhy_camera.set_exposure_max(exposure_max)
-            #     if (gain_min := config_message.get('gain_min') is not None:
-            #         qhy_camera.set_gain_min(gain_min)
-            #     if (gain_max := config_message.get('gain_max') is not None:
-            #         qhy_camera.set_gain_max(gain_max)
-            #     if (exposure_min_step := config_message.get('exposure_min_step') is not None:
-            #         qhy_camera.set_exposure_min_step(exposure_min_step)
-            #     if (gain_min_step := config_message.get('gain_min_step') is not None:
-            #         qhy_camera.set_gain_min_step(gain_min_step)
-            #     if (compensation_factor := config_message.get('compensation_factor') is not None:
-            #         qhy_camera.set_compensation_factor(compensation_factor)
-            #     if (target_temperature := config_message.get('target_temperature') is not None:
-            #         qhy_camera.set_target_temperature(target_temperature)
+        # send status message
+        status_message = {
+            'pulse_time': time.time(),
+            'exposure': qhy_camera.get_exposure(),
+            'gain': qhy_camera.get_gain(),
+            'temperature': qhy_camera.get_temperature()
+        }
+        status_queue.put(status_message)
 
-            # Close the capture
+        # Maybe pass the not None check in the set function
+        while not param_queue.empty():
+            config_message = param_queue.get()
+            qhy_camera.set_target_brightness(config_message['target_brightness'])
+            qhy_camera.set_target_gain(config_message['target_gain'])
+            qhy_camera.set_exposure_min(config_message['exposure_min'])
+            qhy_camera.set_exposure_max(config_message['exposure_max'])
+            qhy_camera.set_gain_min(config_message['gain_min'])
+            qhy_camera.set_gain_max(config_message['gain_max'])
+            qhy_camera.set_exposure_min_step(config_message['exposure_min_step'])
+            qhy_camera.set_gain_min_step(config_message['gain_min_step'])
+            qhy_camera.set_compensation_factor(config_message['compensation_factor'])
+            qhy_camera.set_target_temperature(config_message['target_temperature'])
+
+    # Close the capture
     qhy_camera.close()
 
-def main_controller(channel_name):
+def main_controller():
     param_queue = mp.Queue()
     status_queue = mp.Queue()
     restart_attempts = 0
 
-    # TODO: Add eCAL sender and receiver for status and parameters
+    # Set process name
+    set_process_name(f"{QHY_CHANNEL_NAME} Broadcast")
+
+    # Proto sender
+    status_proto_snd = ProtoSender(QHY_STATUS_CHANNEL, STATUS_MESSAGE_NAME, STATUS_PROTO_FILE)
+
+    # Proto receiver
+    params_proto_rec = ProtoReceiver(QHY_PARAMS_CHANNEL, PARAMS_MESSAGE_NAME, PARAMS_PROTO_FILE)
 
     def start_capture():
         print("[Controller] Start QHY Camera")
-        proc = mp.Process(target=QHYCamera2ecal, args=(channel_name, param_queue, status_queue))
+        proc = mp.Process(target=QHYCamera2ecal, args=(param_queue, status_queue))
         proc.start()
         return proc
 
     capture_proc = start_capture()
 
-    # param_queue.put(camera_params)
+    is_reset_qyc_set = False
+    is_close_qyc_set = False
+
     last_pulse_time = time.time()
     # initial pulse timeout in seconds
     pulse_timeout = INIT_PULSE_TIMEOUT_SEC
@@ -114,33 +122,50 @@ def main_controller(channel_name):
             while not status_queue.empty():
                 status_message = status_queue.get()
 
-                # TODO: publish_status_through_ecal()
-                # status_message['timestamp']
-                # status_message['exposure']
-                # status_message['gain']
-                # status_message['temperature']
+                # Access the protobuf type definition
+                status_proto_message = status_proto_snd.message
+                status_proto_message.temperature = status_message['temperature']
+                status_proto_message.gain = status_message['gain']
+                status_proto_message.exposure = status_message['exposure']
+                status_proto_message.is_qhy_live = True
+                status_proto_message.time_stamp = int(status_message['pulse_time'] * 1.0e6)
+
+                # Send the message to the topic this publisher was created for
+                status_proto_snd.send(status_proto_message)
 
                 last_pulse_time = status_message['pulse_time']
                 pulse_timeout = PULSE_TIMEOUT_SEC
                 restart_attempts = 0
 
-            # TODO: Add receiver from eCAL
-            # if get_param_from_ecal:
-            #     updated_config.target_brightness = message.target_brightness
-            #     updated_config.target_gain = message.target_gain
-            #     updated_config.exposure_min = message.exposure_min
-            #     updated_config.exposure_max = message.exposure_max
-            #     updated_config.gain_min = message.gain_min
-            #     updated_config.gain_max = message.gain_max
-            #     updated_config.exposure_min_step = message.exposure_min_step
-            #     updated_config.gain_min_step = message.gain_min_step
-            #     updated_config.compensation_factor = message.compensation_factor
-            #     updated_config.target_temperature = message.target_temperature
-            #     param_queue.put(updated_config)
+            # Receiver from eCAL
+            if params_proto_rec.wait_for_message(100):
+                params_proto_message = params_proto_rec.message
+                config_message = {
+                    'target_brightness': params_proto_message.target_brightness,
+                    'target_gain': params_proto_message.target_gain,
+                    'exposure_min': params_proto_message.exposure_min,
+                    'exposure_max': params_proto_message.exposure_max,
+                    'gain_min': params_proto_message.gain_min,
+                    'gain_max': params_proto_message.gain_max,
+                    'exposure_min_step': params_proto_message.exposure_min_step,
+                    'gain_min_step': params_proto_message.gain_min_step,
+                    'compensation_factor': params_proto_message.compensation_factor,
+                    'target_temperature': params_proto_message.target_temperature
+                }
+                param_queue.put(config_message)
+                is_reset_qyc_set = params_proto_message.reset_qhy
+                is_close_qyc_set = params_proto_message.close_qhy
 
             # Pulse timeout check
-            if time.time() - last_pulse_time > pulse_timeout:
-                print(f"[Controller] No pulse for {pulse_timeout} sec — restarting QHY Camera")
+            if is_reset_qyc_set or time.time() - last_pulse_time > pulse_timeout:
+                print(f"[Controller] No pulse for {pulse_timeout} sec — restarting Capture")
+
+                # Send the message to the topic
+                status_proto_message = status_proto_snd.message
+                status_proto_message.is_qhy_live = False
+                status_proto_message.time_stamp = int(time.time() * 1.0e6)
+                status_proto_snd.send(status_proto_message)
+
                 capture_proc.terminate()
                 capture_proc.join()
 
@@ -152,29 +177,23 @@ def main_controller(channel_name):
                 capture_proc = start_capture()
                 last_pulse_time = time.time()
                 pulse_timeout = INIT_PULSE_TIMEOUT_SEC
+                is_reset_qyc_set = False
+
+            if is_close_qyc_set:
+                print(f"[Controller] Closing QHY")
+                capture_proc.terminate()
+                break
 
             time.sleep(1)
 
         except KeyboardInterrupt:
-            print("[Controller] Main QHY controller shutdown")
+            print("[Controller] Closing QHY")
             capture_proc.terminate()
             break
 
 if __name__ == "__main__":
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("--channel_name", required=False,
-                    help="Specify the channel name: python QHYCamera2ecal.py --channel_name QHYCamera")
-    args = vars(ap.parse_args())
+    print("\n\npython QHYCamera2ecal.py")
+    print("\n'ctrl+C' to stop\n")
 
-    # Default configurations
-    channel_name = "QHYCamera"
-    debug_view = True
-
-    print("\n\nDefault usage: python QHYCamera2ecal.py --channel_name QHYCamera")
-    print("\nPress 'Esc' key to stop\n")
-
-    if args["channel_name"]:
-        channel_name = args["channel_name"]
-
-    main_controller(channel_name)
+    main_controller()
---
+++ src/camera_monitor_app.py
@@ -0,0 +1,236 @@
+import sys
+import tkinter as tk
+from tkinter import ttk
+import threading
+import time
+import cv2
+import multiprocessing
+
+from ecal_lib.ecal_util.proto_receiver import ProtoReceiver
+from ecal_lib.ecal_util.proto_sender import ProtoSender
+from ecal_lib.ecal_util.image_message import ImageMessage
+from ecal_lib.ecal_util.set_process_name import *
+
+QHY_CHANNEL_NAME = "QHYCamera"
+# MESSAGE_NAME = "web_camera_image"
+# PROTO_FILE = "ecal_lib.proto_files.web_camera_image_pb2"
+MESSAGE_NAME = "qhy_camera_image"
+PROTO_FILE = "ecal_lib.proto_files.qhy_camera_image_pb2"
+
+STATUS_MESSAGE_NAME = "qhy_camera_status"
+STATUS_PROTO_FILE = "ecal_lib.proto_files.qhy_camera_status_pb2"
+QHY_STATUS_CHANNEL = "QHYCamera_status"
+
+PARAMS_MESSAGE_NAME = "qhy_camera_parameters"
+PARAMS_PROTO_FILE = "ecal_lib.proto_files.qhy_camera_parameters_pb2"
+QHY_PARAMS_CHANNEL = "QHYCamera_parameters"
+
+DEFAULT_TARGET_BRIGHTNESS = 0.5
+DEFAULT_TARGET_GAIN = 22
+DEFAULT_EXPOSURE_MIN = 0
+DEFAULT_EXPOSURE_MAX = 110000
+DEFAULT_GAIN_MIN = 1.0
+DEFAULT_GAIN_MAX = 54.0
+DEFAULT_EXPOSURE_MIN_STEP = 50
+DEFAULT_GAIN_MIN_STEP = 0.2
+DEFAULT_COMPENSATION_FACTOR = 0.62
+DEFAULT_TARGET_TEMPERATURE = -20.0 # [°C]
+
+def view_ecal_video():
+    # Set process name
+    set_process_name(f"eCAL viewer on {QHY_CHANNEL_NAME}")
+
+    proto_rec = ProtoReceiver(QHY_CHANNEL_NAME, MESSAGE_NAME, PROTO_FILE)
+    image_msg = ImageMessage(MESSAGE_NAME)
+
+    cv2.namedWindow(QHY_CHANNEL_NAME + " Viewer", cv2.WINDOW_NORMAL)
+
+    while True:
+        if proto_rec.wait_for_message(100):
+            image_msg.process_message(proto_rec.message)
+            frame = image_msg.get_rgb_image()
+
+            # Display the image
+            cv2.imshow(QHY_CHANNEL_NAME + " Viewer", frame)
+
+        k = cv2.waitKey(1)
+        # Esc key to stop
+        if k == 27:
+            break
+
+    # When everything done, release the capture
+    cv2.destroyAllWindows()
+
+class CameraMonitorApp:
+
+    def __init__(self, root):
+        # Initialize eCAL
+        set_process_name(f"{QHY_CHANNEL_NAME} Monitor App")
+
+        # Proto sender
+        self.params_proto_snd = ProtoSender(QHY_PARAMS_CHANNEL, PARAMS_MESSAGE_NAME, PARAMS_PROTO_FILE)
+
+        # Proto receiver
+        self.status_proto_rec = ProtoReceiver(QHY_STATUS_CHANNEL, STATUS_MESSAGE_NAME, STATUS_PROTO_FILE)
+
+        self.root = root
+        root.title("QHY Camera Monitor")
+        root.geometry("780x215")  # Set the starting size of the window
+        root.maxsize(780, 215)  # width x height
+
+        # Create left and right frames
+        left_frame = tk.Frame(root, width=200, height=160)
+        left_frame.grid(row=0, column=0, padx=10, pady=5)
+        self.left_frame = left_frame
+
+        right_frame = tk.Frame(root, width=150, height=160)
+        right_frame.grid(row=0, column=1, padx=10, pady=5)
+
+        msg_frame = tk.Frame(root, width=300,  height=10)
+        msg_frame.place(x=2,  y=185,  relx=0.01,  rely=0.01)
+
+        # GUI elements
+        self.timestamp_var = tk.DoubleVar(value=1000000000000000)
+        self.temp_var = tk.DoubleVar(value=0.0)
+        self.gain_var = tk.DoubleVar(value=0.0)
+        self.exposure_var = tk.DoubleVar(value=0.0)
+
+        self.target_brightness_var = tk.DoubleVar(value=DEFAULT_TARGET_BRIGHTNESS)
+        self.target_gain_var = tk.DoubleVar(value=DEFAULT_TARGET_GAIN)
+        self.exposure_min_var = tk.DoubleVar(value=DEFAULT_EXPOSURE_MIN)
+        self.exposure_max_var = tk.DoubleVar(value=DEFAULT_EXPOSURE_MAX)
+        self.gain_min_var = tk.DoubleVar(value=DEFAULT_GAIN_MIN)
+        self.gain_max_var = tk.DoubleVar(value=DEFAULT_GAIN_MAX)
+        self.exposure_min_step_var = tk.DoubleVar(value=DEFAULT_EXPOSURE_MIN_STEP)
+        self.gain_min_step_var = tk.DoubleVar(value=DEFAULT_GAIN_MIN_STEP)
+        self.compensation_factor_var = tk.DoubleVar(value=DEFAULT_COMPENSATION_FACTOR)
+        self.target_temperature_var  = tk.DoubleVar(value=DEFAULT_TARGET_TEMPERATURE)
+
+        self.support_msg = tk.StringVar(value="")
+        self.camera_msg = tk.StringVar(value="")
+
+        ttk.Label(left_frame, text="Timestamp:").grid(row=0, column=0, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Label(left_frame, textvariable=self.timestamp_var).grid(row=0, column=1, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+
+        ttk.Label(left_frame, text="Temperature (°C):").grid(row=1, column=0, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Label(left_frame, textvariable=self.temp_var).grid(row=1, column=1, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+
+        ttk.Label(left_frame, text="Gain:").grid(row=2, column=0, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Label(left_frame, textvariable=self.gain_var).grid(row=2, column=1, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+
+        ttk.Label(left_frame, text="exposure:").grid(row=3, column=0, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Label(left_frame, textvariable=self.exposure_var).grid(row=3, column=1, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+
+        ttk.Button(left_frame, text="Close QHY", command=self.close_qhy).grid(row=7, column=0)
+        ttk.Button(left_frame, text="Reset QHY", command=self.reset_qhy).grid(row=7, column=1)
+        self.view_button = ttk.Button(left_frame, text="View QHY image", command=self.view_qhy_image)
+        self.view_button.grid(row=8, column=0)
+
+        ttk.Label(right_frame, text="target_brightness:").grid(row=0, column=3, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.target_brightness_var).grid(row=0, column=4)
+        ttk.Label(right_frame, text="target_gain:").grid(row=1, column=3, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.target_gain_var).grid(row=1, column=4)
+        ttk.Label(right_frame, text="exposure_min:").grid(row=2, column=3, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.exposure_min_var).grid(row=2, column=4)
+        ttk.Label(right_frame, text="exposure_max:").grid(row=3, column=3, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.exposure_max_var).grid(row=3, column=4)
+        ttk.Label(right_frame, text="target_temperature:").grid(row=4, column=3, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.target_temperature_var).grid(row=4, column=4)
+
+        ttk.Label(right_frame, text="gain_min:").grid(row=0, column=5, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.gain_min_var).grid(row=0, column=6)
+        ttk.Label(right_frame, text="gain_max:").grid(row=1, column=5, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.gain_max_var).grid(row=1, column=6)
+        ttk.Label(right_frame, text="exposure_min_step:").grid(row=2, column=5, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.exposure_min_step_var).grid(row=2, column=6)
+        ttk.Label(right_frame, text="gain_min_step:").grid(row=3, column=5, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.gain_min_step_var).grid(row=3, column=6)
+        ttk.Label(right_frame, text="compensation_factor:").grid(row=4, column=5, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Entry(right_frame, textvariable=self.compensation_factor_var).grid(row=4, column=6)
+        ttk.Button(right_frame, text="Apply Parameters", command=self.apply_parameters).grid(row=7, column=6)
+        ttk.Label(right_frame, textvariable=self.camera_msg).grid(row=9, column=6, padx=5,  pady=5,  sticky='w'+'e'+'n'+'s')
+        ttk.Label(msg_frame, textvariable=self.support_msg).grid(row=9, columnspan=6)
+
+        # Start status polling
+        self.running = True
+        threading.Thread(target=self.status_listener, daemon=True).start()
+        self.view_process = None
+        # Poll process status
+        self.check_process_status()
+
+    def status_listener(self):
+        while self.running:
+            if self.status_proto_rec.wait_for_message(100):
+                status_msg = self.status_proto_rec.message
+                self.temp_var.set(status_msg.temperature)
+                self.gain_var.set(status_msg.gain)
+                self.exposure_var.set(status_msg.exposure)
+                if status_msg.is_qhy_live:
+                    self.camera_msg.set("QHY is running")
+                else:
+                    self.camera_msg.set("QHY crashed")
+                self.timestamp_var.set(status_msg.time_stamp)
+            time.sleep(0.1)
+
+    def reset_qhy(self):
+        self.camera_msg.set("Resetting QHY")
+        params_message = self.params_proto_snd.message
+        params_message.close_qhy = False
+        params_message.reset_qhy = True
+        self.params_proto_snd.send(params_message)
+
+    def close_qhy(self):
+        self.camera_msg.set("Closing QHY")
+        params_message = self.params_proto_snd.message
+        params_message.close_qhy = True
+        params_message.reset_qhy = False
+        self.params_proto_snd.send(params_message)
+
+    def get_parameters(self, param, param_name, param_default):
+        try:
+            new_param = param.get()
+        except Exception as err:
+            self.support_msg.set(f"{param_name}: {err}")
+            param.set(param_default)
+            new_param = param_default
+        return new_param
+
+    def view_qhy_image(self):
+        # Disable button
+        self.view_button.state(['disabled'])
+        # Start process
+        self.view_process = multiprocessing.Process(target=view_ecal_video)
+        self.view_process.start()
+
+    def check_process_status(self):
+        if self.view_process and not self.view_process.is_alive():
+            self.view_button.state(['!disabled'])  # Re-enable
+            self.view_process = None
+
+        # Re-check every 500 ms
+        self.left_frame.after(500, self.check_process_status)
+
+    def apply_parameters(self):
+        params_message = self.params_proto_snd.message
+        params_message.target_brightness = self.get_parameters(self.target_brightness_var, 'target_brightness', DEFAULT_TARGET_BRIGHTNESS)
+        params_message.target_gain = self.get_parameters(self.target_gain_var, 'target_gain', DEFAULT_TARGET_GAIN)
+        params_message.exposure_min = self.get_parameters(self.exposure_min_var, 'exposure_min', DEFAULT_EXPOSURE_MIN)
+        params_message.exposure_max = self.get_parameters(self.exposure_max_var, 'exposure_max', DEFAULT_EXPOSURE_MAX)
+        params_message.gain_min = self.get_parameters(self.gain_min_var, 'gain_min', DEFAULT_GAIN_MIN)
+        params_message.gain_max = self.get_parameters(self.gain_max_var, 'gain_max', DEFAULT_GAIN_MAX)
+        params_message.exposure_min_step = self.get_parameters(self.exposure_min_step_var, 'exposure_min_step', DEFAULT_EXPOSURE_MIN_STEP)
+        params_message.gain_min_step = self.get_parameters(self.gain_min_step_var, 'gain_min_step', DEFAULT_GAIN_MIN_STEP)
+        params_message.compensation_factor = self.get_parameters(self.compensation_factor_var, 'compensation_factor', DEFAULT_COMPENSATION_FACTOR)
+        params_message.target_temperature = self.get_parameters(self.target_temperature_var, 'target_temperature', DEFAULT_TARGET_TEMPERATURE)
+        params_message.reset_qhy = False
+        params_message.close_qhy = False
+        self.params_proto_snd.send(params_message)
+
+    def shutdown(self):
+        self.running = False
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = CameraMonitorApp(root)
+    root.protocol("WM_DELETE_WINDOW", lambda: (app.shutdown(), root.destroy()))
+    root.mainloop()
